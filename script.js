1d33ade1f34b303c9ba2b5529593446fe3e1d1c2
Eleanor Murguia <Eleanor@Eleanor-Murguias-MacBook-Air.local>
2017-05-30 20:52:35 -0700

    implemented font styling on user elements

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+/*
+ *creating user page 
+ */ 
+
 var content; 
 var type; 
 var page = document.getElementById("userPage");
+var textBox = document.getElementById("textBox"); 
 
 //function to retrive info user entered in form 
 function getInfo(){
@@ -11,6 +16,19 @@ function getInfo(){
 	//console.log(content);
 }
 
+//function to get user selected font 
+function getFont(){ 
+	var fontForm = document.getElementById("fonts");
+	var fontSelection = fontForm.options[fontForm.selectedIndex].value;
+	return fontSelection;
+}
+
+//function to style text 
+function styleText(text){ 
+	text.style.fontFamily = getFont();
+}
+
+
 //function to create text HTML element 
 function createTextElement(tag, info){
 	var newObj = document.createElement(tag);
@@ -18,6 +36,10 @@ function createTextElement(tag, info){
 	var text = document.createTextNode(info);
 	newObj.appendChild(text);
 	page.appendChild(newObj);
+	if (styling) {
+		styleText(newObj);
+	};
+	//return newObj; 
 }
 
 //function to create HTML image 
@@ -26,26 +48,68 @@ function createImage(content){
 	newImg.className = "user";
 	newImg.src = content;
 	page.appendChild(newImg);
+	//return newImg; 
 }
 
-var textBox = document.getElementById("textBox"); 
-var goButton = document.getElementById("goButton");
-var colorPicker = document.getElementById("color");
+/* 
+ * interface 
+ */ 
+
+//function to hide styling options
+function hideStyling(){
+	document.getElementById("stylingOptions").style.display = "none";
+}
+
+//styling form onclick 
+var radio = document.getElementById("radio"); 
+var styling = false; 
+radio.onclick = function(){
+	if(document.getElementById("radio1").checked){
+		hideStyling();
+		styling = false; 
+	}else if(document.getElementById("radio2").checked){
+		document.getElementById("stylingOptions").style.display = "inline";
+		styling = true; 
+	}
+}
+
+
 //function to reset form 
 function resetForm(){
 	document.getElementById("form1").reset();
-	color.value="#ffffff"
+	if(styling){
+		document.getElementById("radio2").checked = true;
+	}
 }
 
-goButton.onclick = function(){
+
+var colorPicker = document.getElementById("colorPicker");
+colorPicker.addEventListener("change", watchColorPicker(), false);
+function watchColorPicker(event){
+		//element.style.color = event.target.value;
+	
+	}
+
+
+var goButton = document.getElementById("goButton");
+goButton.addEventListener("click", addElement, false); 
+//function to add elements to user's page 
+function addElement(){
 	getInfo();
+	//var newElement; 
 	if(type == "h1" || type == "p") {
+		//newElement = 
 		createTextElement(type, content);
+		//console.log(newText);
 	}else if(type = "img"){
+		//newElement = 
 		createImage(content);
+		//console.log(newImage);
 	}
 	resetForm();
 }
+//figure out how to add styling when adding element 
+
 
 //function to clear HTML added by user
 function resetPage(){
@@ -54,6 +118,8 @@ function resetPage(){
 		list[i].parentNode.removeChild(list[i]);
 	};
 	resetForm();
+	hideStyling();
+	document.getElementById("radio1").checked = true;
 }
 
 var resetButton = document.getElementById("resetButton");
@@ -102,4 +168,8 @@ welcomed.onclick = function(){
 	brighten();
 	hideWelcome();
 
+}
+
+window.onload = function(){ 
+	resetForm();
 }
